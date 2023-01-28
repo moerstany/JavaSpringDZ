a1030355627b77db5f9c6db3dae37240d98f0550
moerstany <moerstany@gmail.com>
2023-01-28 22:33:53 +0700

    создали классы

--- src/main/Java/com/topak/springapp/Main.java
+++ src/main/Java/com/topak/springapp/Main.java
@@ -1,7 +1,16 @@
 package com.topak.springapp;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main (String[] args){
         System.out.println("okey");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applocationContext.xml");
+        Автомобиль автомобиль = context.getBean("автомобиль", Автомобиль.class);
+        Двигатель двигатель = context.getBean("двигатель", Двигатель.class);
+        Колесо колесо = context.getBean("колесо", Колесо.class);
+        Кузов кузов = context.getBean("кузов", Кузов.class);
+        Трансмиссия трансмиссия = context.getBean("трансмиссия", Трансмиссия.class);
+
     }
 }
